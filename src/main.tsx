00f75e539007329ab7e28ca8f375edf740fef482
saiful10400 <saifulislam10400i5@gmail.com>
2024-09-13 01:09:13 +0600

    Inetial Router setup done.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./Redux/Store.ts";
+import { RouterProvider } from "react-router-dom";
+import routes from "./Routes/Routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+   <RouterProvider router={routes} />
   </Provider>
 );
