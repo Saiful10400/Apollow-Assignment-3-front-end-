d01d0cc3986e28daffe3b68880008e8f82109d61
saiful10400 <saifulislam10400i5@gmail.com>
2024-09-14 00:19:30 +0600

    advertisement section created.

--- src/Components/Pages/Home/Home.tsx
+++ src/Components/Pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Footer from "../../Layouts/Footer/Footer";
 import Hero from "../../Layouts/Hero/Hero";
+import Advertisement from "../../Layouts/ServiceAdvertisement/Advertisement";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Advertisement/>
       <Footer/>
     </>
   );
