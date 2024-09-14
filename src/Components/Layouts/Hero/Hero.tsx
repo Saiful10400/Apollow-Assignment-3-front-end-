9daa1d6a24e767af212d7b01dce61277fc8f8a01
saiful10400 <saifulislam10400i5@gmail.com>
2024-09-14 22:54:20 +0600

    hero banner is modified.

--- src/Components/Layouts/Hero/Hero.tsx
+++ src/Components/Layouts/Hero/Hero.tsx
@@ -21,8 +21,9 @@ export default function Hero() {
     progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
   };
 
+  const[activeIndex,setActiveIndex]=useState(0)
 
-
+console.log(activeIndex)
 
 const keyboardData=getKeyboardData()
 
@@ -42,10 +43,20 @@ const keyboardData=getKeyboardData()
         modules={[Autoplay, Pagination, Navigation]}
         onAutoplayTimeLeft={onAutoplayTimeLeft}
         className="mySwiper"
+        onSlideChange={(s)=>setActiveIndex(s.activeIndex)}
       >
         {
-            keyboardData?.map((item,idx)=>(
-                <SwiperSlide><img key={idx} src={item.photo} alt="" /></SwiperSlide>
+            keyboardData?.map((item,idx)=>( <SwiperSlide key={idx}>
+               <div key={idx} className='h-[30vh] lg:h-[500px] relative'>
+                <div  className={`absolute bottom-0 left-[5%] delay-75 duration-1000 ${activeIndex===idx?"-translate-y-6 lg:-translate-y-14":"translate-y-0"}`}>
+                  <h1 className='text-white text-xl lg:text-3xl font-semibold lg:font-bold bg-[#5252526c] w-max py-1 lg:py-4 px-1 lg:px-3 rounded-lg'>{item.name}</h1>
+                  <h1 className='text-white mt-2 lg:mt-4 text-base lg:text-xl font-medium lg:font-semibold bg-[#5252526c] w-max py-1 lg:py-4 px-1 lg:px-3 rounded-lg'>{item.status}</h1>
+                </div>
+                <div className={`absolute top-0 right-[5%] delay-75 duration-1000 ${activeIndex===idx?"lg:translate-y-14 translate-y-6":"translate-y-0"}`}>
+                 <img className='lg:w-[100px] object-contain lg:h-[100px] w-[50px] h-[50px] bg-[#5252526c] rounded-lg' src={item.brand} alt="" />
+                </div>
+                <img  className='h-full  w-full object-cover' src={item.photo} alt="" />
+               </div></SwiperSlide>
             ))
         }
        
--- src/Components/Layouts/UserReview/UserReview.tsx
+++ src/Components/Layouts/UserReview/UserReview.tsx
@@ -1,75 +1,71 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import "./style.css"
+import "./style.css";
 
 // import required modules
-import { Pagination, Navigation,Autoplay } from 'swiper/modules';
-import CenterAlign from '../../Helper/CenterAlign';
-import ReviewCard from '../../Ui/ReviewCard';
-import getReviewData from './DemoData';
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import CenterAlign from "../../Helper/CenterAlign";
+import ReviewCard from "../../Ui/ReviewCard";
+import getReviewData from "./DemoData";
 
 export default function UserReview() {
-
-
-
-
-
-
   return (
-   <CenterAlign>
-     <>
-      <Swiper
-       
-     
-        centeredSlides={true}
- 
-        pagination={{
-          type: 'fraction',
-        }}
-        autoplay={{
+    <CenterAlign>
+      <>
+        <Swiper
+          centeredSlides={true}
+          pagination={{
+            type: "custom",
+          }}
+          autoplay={{
             delay: 2500,
             disableOnInteraction: false,
           }}
-        navigation={false}
-        modules={[Pagination, Navigation,Autoplay]}
-        className="mySwiper"
-        breakpoints={{
-          // when window width is >= 640px
-          640: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          // when window width is >= 768px
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          // when window width is >= 1024px
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
-          // when window width is >= 1440px
-          1440: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          }
-        }}
-      >
-        
-       {
-        getReviewData()?.map((item,idx)=><SwiperSlide key={idx}><ReviewCard user={item.user} img={item.photo} ratings={item.review} testimonials={item.testimonial}/></SwiperSlide>)
-       }
-      </Swiper>
-
-      
-    </>
-   </CenterAlign>
+          initialSlide={2}
+          modules={[Pagination, Navigation, Autoplay]}
+          className="mySwiper"
+          breakpoints={{
+            // when window width is >= 640px
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+              
+            },
+            // when window width is >= 768px
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            // when window width is >= 1024px
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            // when window width is >= 1440px
+            1440: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+          }}
+        >
+          {getReviewData()?.map((item, idx) => (
+            <SwiperSlide key={idx}>
+              <ReviewCard
+                user={item.user}
+                img={item.photo}
+                ratings={item.review}
+                testimonials={item.testimonial}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    </CenterAlign>
   );
 }
